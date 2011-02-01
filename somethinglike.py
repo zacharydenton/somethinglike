22ecb3fa8b15ffb0a7df88f5eca434980afb8622
Zach Denton <zacharydenton@gmail.com>
2011-02-01 15:53:42 -0500

    initial commit

---
+++ somethinglike.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# movie recommendations
+import urllib
+import random
+import argparse
+from lxml import etree
+
+class Movie(object):
+    def __init__(self, title):
+        self.title = title
+
+def movies_like(movie_title):
+    url = "http://www.tastekid.com/ask/ws?" + urllib.urlencode({'q' : movie_title})
+    doc = etree.parse(url)
+    for rec in doc.xpath('/similar/results/resource'):
+        yield Movie(rec.xpath('name')[0].text)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--number', help='number of recommendations to display (default: 3)', default=3)
+    parser.add_argument('-r', '--random', action='store_true', help='randomize the results')
+    parser.add_argument('movie_title', help='the title of a movie you like', nargs='+')
+    args = parser.parse_args()
+
+    recommendations = list(movies_like(' '.join(args.movie_title)))
+    if args.random:
+        random.shuffle(recommendations)
+    for movie in recommendations[:args.number]:
+        print movie.title
+
+if __name__ == "__main__": 
+    main()
